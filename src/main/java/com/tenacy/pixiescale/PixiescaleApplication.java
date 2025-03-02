82df90f56c9016738957717f26278687ae191d53
tenacy <tentenacy@gmail.com>
2025-03-02 10:53:47 +0900

    chore: 프로젝트 기본 구조 설정

---
+++ src/main/java/com/tenacy/pixiescale/PixiescaleApplication.java
@@ -0,0 +1,13 @@
+package com.tenacy.pixiescale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixiescaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixiescaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tenacy/pixiescale/PixiescaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenacy.pixiescale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixiescaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
