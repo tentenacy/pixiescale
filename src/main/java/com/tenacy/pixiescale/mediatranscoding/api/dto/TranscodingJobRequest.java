ffaaeeae415a6164d94c742705039bf49611abef
tenacy <tentenacy@gmail.com>
2025-03-14 05:15:54 +0900

    feat: 트랜스코딩 작업 관리 서비스 구현

--- src/main/java/com/tenacy/pixiescale/mediatranscoding/api/dto/TranscodingJobRequest.java
+++ src/main/java/com/tenacy/pixiescale/mediatranscoding/api/dto/TranscodingJobRequest.java
@@ -1,4 +1,16 @@
 package com.tenacy.pixiescale.mediatranscoding.api.dto;
 
+import com.tenacy.pixiescale.mediatranscoding.domain.TranscodingConfig;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TranscodingJobRequest {
+    private String mediaFileId;
+    private TranscodingConfig config;
 }
