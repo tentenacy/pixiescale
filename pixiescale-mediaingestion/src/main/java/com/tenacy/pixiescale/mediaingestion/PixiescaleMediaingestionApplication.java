2b7c39516263ebbafda3f0dbf87e98bbc3ecab19
tenacy <tentenacy@gmail.com>
2025-03-18 08:37:28 +0900

    feat: 미디어 수집 서비스 프로젝트 생성

---
+++ pixiescale-mediaingestion/src/main/java/com/tenacy/pixiescale/mediaingestion/PixiescaleMediaingestionApplication.java
@@ -0,0 +1,13 @@
+package com.tenacy.pixiescale.mediaingestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixiescaleMediaingestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixiescaleMediaingestionApplication.class, args);
+	}
+
+}
