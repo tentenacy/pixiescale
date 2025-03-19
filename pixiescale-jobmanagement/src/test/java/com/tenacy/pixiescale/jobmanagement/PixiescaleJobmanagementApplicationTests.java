eed3ba5fe615e430aa1b1e61516182d2e533250b
tenacy <tentenacy@gmail.com>
2025-03-19 11:26:31 +0900

    feat: 작업 관리 서비스 프로젝트 생성

---
+++ pixiescale-jobmanagement/src/test/java/com/tenacy/pixiescale/jobmanagement/PixiescaleJobmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenacy.pixiescale.jobmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixiescaleJobmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
