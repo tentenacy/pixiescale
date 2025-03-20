e028c9beb98acf35761406b8a908ad1ad6482587
tenacy <tentenacy@gmail.com>
2025-03-20 09:11:26 +0900

    feat: 트랜스코딩 워커 서비스 프로젝트 생성

---
+++ pixiescale-transcodingworker/src/main/java/com/tenacy/pixiescale/transcodingworker/PixiescaleTranscodingworkerApplication.java
@@ -0,0 +1,13 @@
+package com.tenacy.pixiescale.transcodingworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixiescaleTranscodingworkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixiescaleTranscodingworkerApplication.class, args);
+	}
+
+}
---
+++ pixiescale-transcodingworker/src/test/java/com/tenacy/pixiescale/transcodingworker/PixiescaleTranscodingworkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenacy.pixiescale.transcodingworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixiescaleTranscodingworkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
