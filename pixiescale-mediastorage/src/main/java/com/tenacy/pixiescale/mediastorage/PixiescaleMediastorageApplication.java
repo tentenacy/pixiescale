1483bbf0d863d575a5b53f50241aa62e698549a6
tenacy <tentenacy@gmail.com>
2025-03-21 10:42:32 +0900

    feat: 미디어 저장 서비스 프로젝트 생성

---
+++ pixiescale-mediastorage/src/main/java/com/tenacy/pixiescale/mediastorage/PixiescaleMediastorageApplication.java
@@ -0,0 +1,13 @@
+package com.tenacy.pixiescale.mediastorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixiescaleMediastorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixiescaleMediastorageApplication.class, args);
+	}
+
+}
---
+++ pixiescale-mediastorage/src/test/java/com/tenacy/pixiescale/mediastorage/PixiescaleMediastorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenacy.pixiescale.mediastorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixiescaleMediastorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
