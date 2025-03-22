70b849401a321f175280815e570aa581d7b5082b
tenacy <tentenacy@gmail.com>
2025-03-22 11:04:01 +0900

    feat: API 게이트웨이 프로젝트 생성

---
+++ pixiescale-apigateway/src/main/java/com/tenacy/pixiescale/apigateway/PixiescaleApigatewayApplication.java
@@ -0,0 +1,13 @@
+package com.tenacy.pixiescale.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixiescaleApigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixiescaleApigatewayApplication.class, args);
+	}
+
+}
---
+++ pixiescale-apigateway/src/test/java/com/tenacy/pixiescale/apigateway/PixiescaleApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenacy.pixiescale.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixiescaleApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
