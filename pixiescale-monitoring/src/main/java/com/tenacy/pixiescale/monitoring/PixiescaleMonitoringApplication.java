fec4483dc1f79da3b6318698c9542042687a849e
tenacy <tentenacy@gmail.com>
2025-03-23 14:08:19 +0900

    feat: 모니터링 서비스 프로젝트 생성

---
+++ pixiescale-monitoring/src/main/java/com/tenacy/pixiescale/monitoring/PixiescaleMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.tenacy.pixiescale.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixiescaleMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixiescaleMonitoringApplication.class, args);
+	}
+
+}
---
+++ pixiescale-monitoring/src/test/java/com/tenacy/pixiescale/monitoring/PixiescaleMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenacy.pixiescale.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixiescaleMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
